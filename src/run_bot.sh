50f09191c4fde420c7a3844d6e81d2f9c7dcbb6f
bruno <brunopaes05@gmail.com>
2021-02-18 21:03:57 -0300

    Documentation on it

--- src/run_bot.sh
+++ src/run_bot.sh
@@ -1,6 +1,8 @@
+# 0 * * * * bash /home/ec2-user/muunilinst-igbc/src/run_bot.sh
+
 # Accessing btc dir
 cd /home/ec2-user/muunilinst-igbc/src
 
 # Activating python
 python3 btc_inserter.py
-python3 btc_messager.py
+python3 btc_courier.py
